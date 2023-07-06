c60ca9f1f839c2ea01025133371d8e94b5d37809
Haziq Khairi <ikhairulhaziq11@gmail.com>
2023-07-06 15:22:17 +0800

    feat: change to 1D array

--- src/main.cpp
+++ src/main.cpp
@@ -20,19 +20,13 @@ certain number of these coupons qualifies them for college dorm admission.
 
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
+#include <cstring>
 #include <sstream>
 #include <stdexcept>
 
-const int MAX_ACTIVITIES = 10;
-const int MAX_STUDENTS = 50;
-
 using namespace std;
 
-const string STUDENTS_FILE = "../data/students.txt";
-const string ACTIVITIES_FILE = "../data/activities.txt";
-
+// Struct
 struct Student
 {
     string id;
@@ -46,65 +40,44 @@ struct Activity
     string name;
 };
 
-vector<Student> loadStudents()
-{
-    ifstream file(STUDENTS_FILE);
-    vector<Student> students;
-    string line;
-    while (getline(file, line))
-    {
-        stringstream ss(line);
-        string id, name;
-        int coupons;
-        getline(ss, id, ',');
-        getline(ss, name, ',');
-        ss >> coupons;
-        students.push_back({id, name, coupons});
-    }
-    return students;
-}
+// Global Variables
+const int MAX_ACTIVITIES = 10;
+const int MAX_STUDENTS = 50;
+const string STUDENTS_FILE = "../data/students.txt";
+const string ACTIVITIES_FILE = "../data/activities.txt";
 
-void saveStudents(const vector<Student> &students)
-{
-    ofstream file(STUDENTS_FILE);
-    for (const Student &student : students)
-    {
-        file << student.id << ',' << student.name << ',' << student.coupons << '\n';
-    }
-}
+int numStudents = 0;
+Student students[MAX_STUDENTS];
 
-Student *findStudentById(const string &id, vector<Student> &students)
-{
-    for (Student &student : students)
-    {
-        if (student.id == id)
-            return &student;
-    }
-    return nullptr;
-}
+int numActivities = 0;
+Activity activities[MAX_ACTIVITIES];
 
-// Function to read student information from a file
-void readStudentInfo(const Student students[], int numStudent)
+// User-defined Functions
+void loadStudents()
 {
-    ifstream inputS;
-    inputS.open(STUDENTS_FILE);
+    ifstream inputFile(STUDENTS_FILE);
     if (!inputFile)
     {
         throw runtime_error("Unable to open activities file for reading.");
     }
-    int i = 0;
-    while (!inputS.eof() && i < numStudent)
-    {
-        inputS >> students[i].id;
-        getline(inputS, students[i].name, ',');
-        inputS >> students[i].coupons;
-        cin.ignore();
-        i++;
-    }
-    inputS.close();
-}
 
-void readActivityInfoFromFile(Activity activities[], int &numActivities)
+    string line;
+    while (getline(inputFile, line))
+    {
+        stringstream ss(line);
+        string id, name;
+        int coupons;
+
+        getline(ss, id, ',');
+        getline(ss, name, ',');
+        ss >> coupons;
+
+        students[numStudents] = {id, name, coupons};
+        numStudents++;
+    }
+    inputFile.close();
+}
+void loadActivities()
 {
     ifstream inputFile(ACTIVITIES_FILE);
     if (!inputFile)
@@ -125,17 +98,37 @@ void readActivityInfoFromFile(Activity activities[], int &numActivities)
     inputFile.close();
 }
 
-void viewAllStudents(const vector<Student> &students)
+void saveStudents()
+{
+    ofstream outputFile(STUDENTS_FILE);
+    for (int i = 0; i < numStudents; i++)
+    {
+        outputFile << students[i].id << ',' << students[i].name << ',' << students[i].coupons << '\n';
+    }
+    outputFile.close();
+}
+
+Student *findStudentById(const string &id)
+{
+    for (int i = 0; i < numStudents; i++)
+    {
+        if (students[i].id == id)
+            return &students[i];
+    }
+    return nullptr;
+}
+
+void viewAllStudents()
 {
     cout << "Registered Students:\n";
-    for (const Student &student : students)
+    for (int i = 0; i < numStudents; i++)
     {
-        cout << "ID: " << student.id << ", Name: " << student.name << ", Coupons: " << student.coupons << '\n';
+        cout << "ID: " << students[i].id << ", Name: " << students[i].name << ", Coupons: " << students[i].coupons << '\n';
     }
     cout << endl;
 }
 
-void viewAllActivities(const Activity activities[], int numActivities)
+void viewAllActivities()
 {
     cout << "Registered Activities:\n";
     for (int i = 0; i < numActivities; i++)
@@ -159,15 +152,8 @@ void displayMenu()
 
 int main()
 {
-    vector<Student> students = loadStudents();
-    Activity activityInfo[MAX_ACTIVITIES];
-    Student students[MAX_STUDENTS];
-
-    int numStudents;
-    int numActivities = 0;
-
-    readActivityInfoFromFile(activityInfo, numActivities);
-    readStudentInfo(students, numStudents);
+    loadStudents();
+    loadActivities();
 
     while (true)
     {
@@ -185,8 +171,9 @@ int main()
             cout << "Enter student name: ";
             getline(cin, name);
 
-            students.push_back({id, name, 0});
-            saveStudents(students);
+            students[numStudents] = {id, name, 0};
+            numStudents++;
+            saveStudents();
 
             cout << "Student registered!\n";
             cout << endl;
@@ -196,11 +183,11 @@ int main()
             string id;
             cout << "Enter student ID: ";
             cin >> id;
-            Student *student = findStudentById(id, students);
+            Student *student = findStudentById(id);
             if (student)
             {
                 student->coupons++;
-                saveStudents(students);
+                saveStudents();
                 cout << "Coupon added!\n";
             }
             else
@@ -214,7 +201,7 @@ int main()
             string id;
             cout << "Enter student ID: ";
             cin >> id;
-            Student *student = findStudentById(id, students);
+            Student *student = findStudentById(id);
             if (student)
             {
                 if (student->coupons >= 10)
@@ -234,11 +221,11 @@ int main()
         }
         else if (choice == 4)
         {
-            viewAllStudents(students);
+            viewAllStudents();
         }
         else if (choice == 5)
         {
-            viewAllActivities(activityInfo, numActivities);
+            viewAllActivities();
         }
         else if (choice == 6)
         {
